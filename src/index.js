c55b6a64ec8ed7db99967f6bbb231e43b51629a2
kerri walker <kerriannwalker3@gmail.com>
2020-04-26 12:33:52 -0400

    Add redux reducers, actions and store to handle employee info and interests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import testFunc from './another';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 console.log('Kerri loves to bike outside. Everyday!');
 
@@ -9,20 +9,70 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-const reducer = function (state = {}, action) {
-  if ('USER_NAME' === action.type) {
-    return action.payload;
+const reducerEmployee = (state = {}, action) => {
+  switch (action.type) {
+    case 'TEAM_MEMBER': {
+      state = { ...state, employee: action.payload };
+      break;
+    }
+    case 'POSITION': {
+      state = { ...state, position: action.payload };
+      break;
+    }
   }
   return state;
 };
 
-const store = createStore(reducer);
+const reducerEmployeeInterests = (state = {}, action) => {
+  switch (action.type) {
+    case 'HOBBIES': {
+      state = { ...state, hobbies: action.payload };
+      break;
+    }
+  }
+  return state;
+};
 
+//here we combine reducers before passing them to the store
+const reducers = combineReducers({
+  employee: reducerEmployee,
+  interests: reducerEmployeeInterests
+});
+
+const store = createStore(reducers);
+
+//subscribe to store
 store.subscribe(() => {
   console.warn('My store has changed', store.getState());
 });
 
-store.dispatch({
-  type: 'TEAM_MEMBER',
-  payload: 'Kerri Walker'
-});
+//action creator function
+
+function getTeamMember() {
+  return {
+    type: 'TEAM_MEMBER',
+    payload: 'Kerri Walker'
+  };
+}
+
+function getPosition() {
+  return {
+    type: 'POSITION',
+    payload: 'Software Developer'
+  };
+}
+
+function getInterests() {
+  return {
+    type: 'HOBBIES',
+    payload: 'Running, Hiking, Biking, Lifting, Learning, Reading, Writing'
+  };
+}
+
+store.dispatch(getTeamMember());
+
+//dispatch action to reducer function
+
+store.dispatch(getPosition());
+
+store.dispatch(getInterests());
